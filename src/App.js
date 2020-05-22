55db38c64e5a4e715c7162c86480c49f30586a5a
v_ignatov <inaburat@gmail.com>
2020-05-22 23:31:33 +0300

    initial project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello world</h1>
+			<Button color="primary">Hello again!</Button>
+		</div>
+	);
+}
+
+export default App;
