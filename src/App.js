d18fad2e0d339a32fc55974e396e4c64b0dee985
v_ignatov <inaburat@gmail.com>
2020-05-23 03:44:01 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React from 'react'
+import { Button } from '@material-ui/core'
+import axios from 'axios'
+import MenuAppBar from './components/MenuAppBar'
 
-function App() {
-	return (
-		<div>
-			<h1>Hello world</h1>
-			<Button color="primary">Hello again!</Button>
-		</div>
-	);
+
+class App extends React.Component {
+	handleClick = () => {
+		axios.get('https://api.github.com/')
+			.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+	}
+	render() {
+		return (
+			<div>
+				<MenuAppBar />
+				<h1>Hello world</h1>
+				<Button color="primary" onClick={this.handleClick}>Hello again!</Button>
+			</div>
+		)
+	}
 }
 
 export default App;
